b456dc177937577e3693c4628f46d67042733338
Carl Bäckström <genzorg@gmail.com>
2026-01-13 12:17:21 +0100

    chore: package maintenance

--- jest.config.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  displayName: 'templater',
-  transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
-  },
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../coverage/templater',
-  testPathIgnorePatterns: ['/node_modules/', '/dist/'], 
-};
